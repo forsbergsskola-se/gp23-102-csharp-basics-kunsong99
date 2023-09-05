79da8a2212afc239812212d274c94204eadf516e
somrar <kun.song@student.forsbergsskola.se>
2023-09-05 11:24:52 +0200

    Create P4Comments

---
+++ P4Comments/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Author: Kun Song
+// Date: 2023-09-05
+// Email: Kun.song@student.forsbergsskola.se
+Console.WriteLine("Hello, World!");
