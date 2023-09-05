b6aef2f47f5ccdc435044b0ae013c9b69b83ea27
somrar <kun.song@student.forsbergsskola.se>
2023-09-05 10:29:28 +0200

    Create P3PrintingOutput and Update P2CodeExecution

---
+++ P3PrintingOutput/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Author: KunSong
+//Date: 2023-09-05
+//E-mail: kun.song@student.forsbergsskola.se
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to 102 CSharp Basics!");
+Console.WriteLine("It's easy to print output...");
+Console.WriteLine("...to the console!");
