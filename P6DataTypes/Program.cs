3b7291a6dfa024aceb0796591e79c380ad0dc47f
somrar <kun.song@student.forsbergsskola.se>
2023-09-05 21:16:07 +0200

    Use \n and @ in printing

--- P6DataTypes/Program.cs
+++ P6DataTypes/Program.cs
@@ -1,14 +1,28 @@
-﻿var outputString = "Hello world";
+﻿using System.Globalization;
+
+var outputString = "Hello world";
 Console.WriteLine(outputString);
 
-float decimalValue = 0.3f;
+float decimalValue = 0.3f; //(0.3f)
 Console.WriteLine(decimalValue); 
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
 bool isAwesome = true;
 Console.WriteLine(isAwesome);
 
 byte hexByte = 0xa7;
 Console.WriteLine(hexByte);
+
+Console.WriteLine("***solution 2:");
+Console.WriteLine($"{outputString}\n{decimalValue}\n{isAwesome}\n{hexByte}");
+
+Console.WriteLine("***solution 3:");
+Console.WriteLine(
+    @$"{outputString}
+{decimalValue}
+{isAwesome}
+{hexByte}");
+
 
 // //use of $
 // string firstName = "Astrid";
