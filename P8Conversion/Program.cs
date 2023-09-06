025c113572205b312fb9d0d5f0d7684c4fdf2ddd
somrar <kun.song@student.forsbergsskola.se>
2023-09-06 16:30:53 +0200

    Create P8Conversion

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Give me a number.");
+var inputString = Console.ReadLine();
+Console.WriteLine(inputString);
+double toDouble = Convert.ToDouble(inputString);
+Console.WriteLine(toDouble);
+int toInt = Convert.ToByte(toDouble);
+Console.WriteLine(toInt);
+toInt = Convert.ToInt16(inputString);  //Unhandled exception. System.FormatException: The input string '9.9' was not in a correct format.
+Console.WriteLine(toInt);  // this line will not be executed
