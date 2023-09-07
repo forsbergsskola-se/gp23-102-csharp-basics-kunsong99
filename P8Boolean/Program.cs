703d34b062e6f88861760197fe17a8888904d19b
somrar <kun.song@student.forsbergsskola.se>
2023-09-07 13:37:39 +0200

    Update P8Boolen

--- P8Boolean/Program.cs
+++ P8Boolean/Program.cs
@@ -1,9 +1,9 @@
-﻿Console.WriteLine("Please input your age");
+﻿Console.WriteLine("What is your age?");
 string userInput = Console.ReadLine();
 
 double age = double.Parse(userInput);
 
-bool isChild = age >= 0 && age <=12;
+bool isChild = age >= 0 && age <13;
 bool isTeenager = age >=13 && age <=19;
 bool isGrownup = age > 19;
 
