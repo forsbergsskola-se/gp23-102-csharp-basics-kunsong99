d1aaada8d8b1f9935ffae7c9bf2452d8a0ee1adb
somrar <kun.song@student.forsbergsskola.se>
2023-09-07 09:43:15 +0200

    Create Exercise3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Write a program that calculates the remainder of dividing two integers and displays the result.
+Console.WriteLine("Please input two integers as Dividend and Divisor!");
+string userInput1 = Console.ReadLine();
+string userInput2 = Console.ReadLine();
+
+int dividend = int.Parse(userInput1);
+int divisor = int.Parse(userInput2); 
+
+int reminder = dividend % divisor;
+Console.WriteLine($"the reminder of {userInput1} / {userInput2} is {reminder}");
