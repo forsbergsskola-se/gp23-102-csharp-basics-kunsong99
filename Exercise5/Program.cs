e1d4aec403aa4b45119c142d6200fb5fa01f7e87
somrar <kun.song@student.forsbergsskola.se>
2023-09-07 10:01:02 +0200

    Create Excercise 4-7

---
+++ Exercise5/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Write a program that takes an integer input
+and displays its negation using the unary minus operator.*/
+
+Console.WriteLine("Please input a number");
+string userInput = Console.ReadLine();
+
+double number = double.Parse(userInput);
+
+double negation = 0-number;
+
+Console.WriteLine($"the negation of {userInput} is {negation}");
---
+++ Exercise7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Develop a program that calculates the BMI of a person using their weight in kilograms and height in meters.
+// Display the BMI value: the body mass divided by the square of the body height,
+
+Console.WriteLine("Please input your weight (kg) and height (m)!");
+string userInput1 = Console.ReadLine();
+string userInput2 = Console.ReadLine();
+
+double weight = double.Parse(userInput1);
+double height = double.Parse(userInput2); 
+
+double resultBMI = weight/height/height;
+
+Console.WriteLine($"Your BMI is {resultBMI}");
