05f822b34b11a075848f0c8c07605e9d9a238464
somrar <kun.song@student.forsbergsskola.se>
2023-09-07 13:09:17 +0200

    Create ExerciseBonus1

---
+++ ExerciseBonus1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Write a Program that toggles a 0/1 Input.
+// Do not use conditional code, only mathematical operations.
+// (Input: 0: Output 1 and Input 1: Output 0
+
+Console.WriteLine("Please input 0 or 1!");
+string userInput = Console.ReadLine();
+
+int inputNumer  = int.Parse(userInput);
+
+int outputNumber = 1-inputNumer;
+
+Console.WriteLine($"{userInput} --> {outputNumber}");
