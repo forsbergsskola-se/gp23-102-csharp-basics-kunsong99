8b3df4bf934abfaf5f29e7de50ba1f7e929e3121
somrar <kun.song@student.forsbergsskola.se>
2023-09-11 19:04:07 +0200

    Update
    Let AI go first

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -1,25 +1,62 @@
-﻿//the Player who has to take last match loses
-Console.WriteLine("Welcome to Nim!");
+﻿//24 mathes and 2 players take turns to draw matches
+//each player may draw 1,2 or 3 matches (not more or less)
+//the Player who has to take last match loses
 
-// TODO: This is hard-coded for now. How to do better?
-string draw = "|";
+Console.WriteLine("Welcome to Nim!");
+string match = "|";
 int drawLeft = 24;
-int i = 0;
+int i;
+int aiDrawNumber;
+string userInput;
 
 for (i = 0; i < drawLeft; i++)
 {
-    Console.Write(draw);
+    Console.Write(match);
 }
-Console.Write($"({drawLeft})\n");
-//Console.WriteLine("|||||||||||||||||||||||| (24)");
+Console.Write($"( {drawLeft})\n");
 
-object random;
+
+Console.WriteLine("AI goes first.");
 while (drawLeft >1)
 {
+    //AI's turn
+    // Random rnd = new Random();
+    if (drawLeft > 4)
+    {
+        if (drawLeft % 4 > 1  )
+        {
+            aiDrawNumber = drawLeft % 4 - 1; 
+        }
+        // else if (drawLeft % 4 == 1)
+        // {
+        //     aiDrawNumber =  rnd.Next(1, 3) ; 
+        // }
+        else
+        {
+            aiDrawNumber = 3 ; 
+        }
+    }
+    else
+    {
+        aiDrawNumber = drawLeft - 1;
+    }
+    
+    drawLeft -= aiDrawNumber;
+    Console.WriteLine($"The AI draws {aiDrawNumber} matches.");
+    for (i = 0; i < drawLeft; i++)
+    {
+        Console.Write(match);
+    }
+    Console.Write($" ({drawLeft})\n");
+    if (drawLeft == 1)
+    {
+        Console.WriteLine("You lose!");
+        break;
+    }
+    
     USER_TURN:
     Console.WriteLine("How many matches do you want to draw?");
-    string userInput = Console.ReadLine();
-    //TODO: user input should also less than drawLeft
+    userInput = Console.ReadLine();
     if (userInput != "1" && userInput != "2" && userInput != "3")
     {
         Console.WriteLine("Invalid input. Please input a number between 1 and 3.");
@@ -27,31 +64,15 @@ while (drawLeft >1)
     }
 
     int userDrawNumber = int.Parse(userInput);
-    drawLeft = drawLeft - userDrawNumber;
+    drawLeft -= userDrawNumber;
     for (i = 0; i < drawLeft; i++)
     {
-        Console.Write(draw);
+        Console.Write(match);
     }
-    Console.Write($"({drawLeft})\n");
+    Console.Write($" ({drawLeft})\n");
     if (drawLeft == 1)
     {
         Console.WriteLine("You win!");
-        break;
-    }
-
-    //TODO: user input should also less than drawLeft
-    Random rnd = new Random();
-    int aiDrawNumber = rnd.Next(1, 4); 
-    drawLeft = drawLeft - aiDrawNumber;
-    Console.WriteLine($"AI draws {aiDrawNumber} matches.");
-    for (i = 0; i < drawLeft; i++)
-    {
-        Console.Write(draw);
-    }
-    Console.Write($"({drawLeft})\n");
-    if (drawLeft == 1)
-    {
-        Console.WriteLine("You lose!");
         break;
     }
 }
